14301fb39e470d4e7700775ddfe4a45d07d79956
Alessandro Campo <alessandrocampo97@gmail.com>
2024-04-22 19:23:59 +0200

    feat: first api call for home

--- boolbnb_frontend/src/views/HomeView.vue
+++ boolbnb_frontend/src/views/HomeView.vue
@@ -1,19 +1,36 @@
-<script setup>
-  import NavBar from '../components/NavBar.vue'
-  import Cards from '../components/Cards.vue'
-</script>
-
 <template>
   <header>
-    <NavBar/>
+    <NavBar />
   </header>
-  <main class="p-16 flex flex-wrap gap-20 justify-center">
-      <div v-for="index in 15" :key="index">
-        <Cards/>
-      </div>
-    </main>
-  </template>
+  <section class="p-16 flex flex-wrap gap-20 justify-between">
 
-<style scoped>
+    <Cards v-for="(accomodation, index) in api_reponse" :key="accomodation.id" :prop_accomodation="accomodation" />
+  </section>
 
-</style>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+import Cards from '../components/Cards.vue'
+import { useApiStore } from '@/stores/apiStore'
+
+export default {
+  data() {
+    return {
+      title: 'hello',
+      api_store: useApiStore(),
+      api_reponse: undefined
+    }
+  },
+  components: { NavBar, Cards },
+  async mounted() {
+    let fetched_accomodations = await this.api_store.getHomeAccomodations(1);
+    this.api_reponse = fetched_accomodations;
+    console.log(this.api_reponse)
+
+
+  }
+}
+</script>
+
+<style scoped></style>
