9bf4941c5792b802ffe62ff435d57ae1df08d30f
DavideTarantino <davidetarantino2005@gmail.com>
2024-04-22 17:00:03 +0200

    app vue fix

--- boolbnb_frontend/src/App.vue
+++ boolbnb_frontend/src/App.vue
@@ -1,26 +1,12 @@
-<script>
+<script setup>
   import { RouterLink, RouterView } from 'vue-router'
-
-  export default{
-    components: {
-      NavBar,
-      Cards
-    },
-    data(){
-      return{}
-    }
-  }
 </script>
 
 <template>
   <header>
     <RouterView />
   </header>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </nav>
-  </template>
+</template>
 
 <style scoped>
 
