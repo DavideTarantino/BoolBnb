069bcbcd26cbc159c0a687d50453572867f92ebf
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-04-26 15:55:54 +0200

    fixed: accommodation page error

--- boolbnb_frontend/src/router/index.js
+++ boolbnb_frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import AccommodationPage from '../views/AccommodationPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
