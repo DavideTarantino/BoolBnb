211a38b47bda9c9d607f4bda72fa0fee56772406
Alessandro Campo <alessandrocampo97@gmail.com>
2024-04-26 19:30:43 +0200

    fix:removed searchbar outline

--- boolbnb_frontend/src/components/NavBar.vue
+++ boolbnb_frontend/src/components/NavBar.vue
@@ -2,23 +2,37 @@
     <main>
         <header class="flex justify-between pr-32 pl-32 py-5 border-b-2 border-black items-center">
             <a href="#"><img class="w-28" src="../../public/monsterbnb-logo.svg" alt="logo"></a>
-            <div class="flex items-center gap-2  dropdown">
-                <div
-                    class="search-group flex items-center justify-between border-2 rounded-full w-[500px] px-3 pl-6 py-2 ">
-                    <input class="w-4/5" type="search" placeholder="Search accomodation..." v-model="search_string"
-                        @input="getAddressReccomandations">
-                    <i class="fa-solid fa-magnifying-glass" style="color: #bdc6d6;" @click="searchAccomodations"></i>
+            <div class="flex items-center gap-2">
+                <div class="flex items-center gap-2  dropdown">
+                    <div
+                        class="search-group flex items-center justify-between border-2 rounded-full w-[500px] px-3 pl-6 py-2 ">
+                        <input class="w-4/5" type="search" placeholder="Search accomodation..." v-model="search_string"
+                            @input="getAddressReccomandations">
+                        <i class="fa-solid fa-magnifying-glass" style="color: #bdc6d6;"
+                            @click="searchAccomodations"></i>
+
+                    </div>
+
+
+                    <!-- <a href="#" class="p-2 border-2 rounded-full"></a> -->
+                    <div class="dropdown-content" style="display: block;"
+                        v-show="search_string && address_suggestions.length > 0">
+                        <span v-for="(suggestion, index) in address_suggestions" :key="index"
+                            @click="setReaserch(suggestion)">
+                            {{ suggestion?.address }}
+                        </span>
+                    </div>
+
                 </div>
 
-                <!-- <a href="#" class="p-2 border-2 rounded-full"></a> -->
-                <div class="dropdown-content" style="display: block;"
-                    v-show="search_string && address_suggestions.length > 0">
-                    <span v-for="(suggestion, index) in address_suggestions" :key="index"
-                        @click="setReaserch(suggestion)">
-                        {{ suggestion?.address }}
-                    </span>
-                </div>
+                <div class="p-3 border-2 rounded-full cursor-pointer"><svg class="w-5"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path
+                            d="M0 416c0 17.7 14.3 32 32 32l54.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 448c17.7 0 32-14.3 32-32s-14.3-32-32-32l-246.7 0c-12.3-28.3-40.5-48-73.3-48s-61 19.7-73.3 48L32 384c-17.7 0-32 14.3-32 32zm128 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM320 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32-80c-32.8 0-61 19.7-73.3 48L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l246.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48l54.7 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-54.7 0c-12.3-28.3-40.5-48-73.3-48zM192 128a32 32 0 1 1 0-64 32 32 0 1 1 0 64zm73.3-64C253 35.7 224.8 16 192 16s-61 19.7-73.3 48L32 64C14.3 64 0 78.3 0 96s14.3 32 32 32l86.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 128c17.7 0 32-14.3 32-32s-14.3-32-32-32L265.3 64z" />
+                    </svg></div>
+
             </div>
+
             <div class="flex items-center gap-4">
                 <p><strong>Airbnb your home</strong></p>
                 <div class="dropdown">
@@ -48,6 +62,10 @@
 <style scoped>
 header {
     border-bottom: 2px solid #DDDDDD;
+}
+
+input {
+    outline: none;
 }
 
 .dropdown {
